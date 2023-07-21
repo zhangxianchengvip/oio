82e45ada068f1c520dbf1ec8b26fc0f7b45a9ea8
zhangxiancheng <xian_cheng@ckc.creative.com>
2023-07-21 19:57:09 +0800

    init

---
+++ src/oio.Core/OioCoreServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace oio.Core
+{
+    public static class OioCoreServiceExtensions
+    {
+        public static IServiceCollection AddOioCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
